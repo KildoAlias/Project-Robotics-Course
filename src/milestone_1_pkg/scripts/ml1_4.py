63a3b7a1849e5e63f7c140550bcba74fad1f4809
KildoAlias <kildo.alias@live.com>
2020-02-05 15:07:39 +0100

    Added nodes

--- src/milestone_1_pkg/scripts/ml1_4.py
+++ src/milestone_1_pkg/scripts/ml1_4.py
@@ -19,20 +19,26 @@ from crazyflie_driver.msg import Position, Hover
 state = 0
 angle = 0
 count = 0
+count_pose = 0
+Xm = input("How many meters: ")
+N = input("How many laps: ")
+
+def init_pose(msg):
+    global x_init, y_init
+
+    x_init = msg.pose.position.x
+    y_init = msg.pose.position.y
+
+
 
 def cmd_func(msg):
-    global state, angle, count
-    
-    Xm = 0
-    z_dist = 0.5
-    N = 1
+    global state, angle, count, count_pose, Xm, N
 
-    hov = Hover()
-    hov.header.stamp = rospy.Time.now()
-    hov.header.frame_id = msg.header.frame_id
-    hov.vx = 0 #msg.pose.position.x
-    hov.vy = 0 #msg.pose.position.y
-    hov.zDistance = z_dist #msg.pose.position.z
+    if count_pose < 1:
+        init_pose(msg)
+        count_pose = 1
+
+    z_dist = 0.5
 
     diff_z = abs(msg.pose.position.z - z_dist)
     diff_x = abs(msg.pose.position.x - Xm)
@@ -42,19 +48,19 @@ def cmd_func(msg):
     cmd.header.frame_id = msg.header.frame_id
 ##############################################################
     if state == 0:
-        cmd.x = 0
-        cmd.y = 0
+        cmd.x = x_init
+        cmd.y = y_init
         cmd.z = z_dist
         cmd.yaw = 0
         pub_cmd.publish(cmd)
-        # rospy.loginfo(cmd)
+
         if diff_z < 0.05:
             rospy.sleep(0.2)
             state = 1
 ##############################################################      
     if state == 1:
-        cmd.x = Xm
-        cmd.y = 0
+        cmd.x = x_init + Xm
+        cmd.y = y_init
         cmd.z = z_dist
         cmd.yaw = 0
         pub_cmd.publish(cmd)
@@ -64,133 +70,52 @@ def cmd_func(msg):
             state = 3
 ##################################################################
     if state == 3:
-        cmd.x = Xm
-        cmd.y = 0
+        cmd.x = x_init + Xm
+        cmd.y = y_init
         cmd.z = z_dist
         angle += 2
 
         if angle == 360:
             angle = 0
             count += 1
+            rospy.loginfo("Lap: " + str(count))
+        cmd.yaw = angle
+        pub_cmd.publish(cmd)
 
         if count == N:
             if angle == 180:
                 state = 4
                 rospy.sleep(0.2)
 
-        cmd.yaw = angle
-        rospy.loginfo(cmd.yaw)
-        pub_cmd.publish(cmd)
 ##################################################################
     if state == 4:
-        cmd.x = 0
-        cmd.y = 0
+        cmd.x = x_init
+        cmd.y = y_init
         cmd.z = z_dist
         cmd.yaw = 180
         pub_cmd.publish(cmd)
 
 
-
-        
-
-
-    # rospy.loginfo(diff_x)
-    # if diff_x < 0.2 and diff_y < 0.2 and state == 1:
-    #     idnt = random.randrange(1, 9)
-    #     state = 2
-    #     diff_x = 99
-    #     rospy.loginfo(idnt)
-
-    # if diff_x < 0.2 and diff_y < 0.2 and state == 2:
-    #     idnt = random.randrange(1, 9)
-    #     state = 3
-    #     diff_x = 99
-    
-    # if diff_x < 0.2 and diff_y < 0.2 and state == 3:
-    #     idnt = random.randrange(1, 9)
-    #     state = 1
-    #     diff_x = 99
+################################################################
 
 
-rospy.init_node('moveit')
+
+
+
+####################################################################
+####################################################################
+
+#### MAIN ############################
+
+#####################################################################
+######################################################################
+
+rospy.init_node('ml1_4')
 sub_goal = rospy.Subscriber('/cf1/pose', PoseStamped, cmd_func)
 pub_cmd  = rospy.Publisher('/cf1/cmd_position', Position, queue_size=2)
-pub_hover  = rospy.Publisher('/cf1/cmd_hover', Hover, queue_size=2)
 tf_buf   = tf2_ros.Buffer()
 tf_lstn  = tf2_ros.TransformListener(tf_buf)
 
 
 if __name__ == "__main__":
-    global state, angle, count
-
     rospy.spin()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#     def cmd_func(msg):
-#     global idnt, state
-    
-#     rospy.loginfo(msg)
-#     # frame_c = 'aruco/marker' + str(idnt)
-#     # trans = tf_buf.lookup_transform('map', frame_c, rospy.Time(), rospy.Duration(1.0) )
-#     cmd = Hover()
-
-#     cmd.header.stamp = rospy.Time.now()
-#     cmd.header.frame_id = msg.header.frame_id
-#     cmd.vx = 0 #msg.pose.position.x
-#     cmd.vy = 0 #msg.pose.position.y
-#     cmd.zDistance = 1 #msg.pose.position.z 
-#     # roll, pitch, yaw = euler_from_quaternion((trans.transform.rotation.x,
-#     #                                           trans.transform.rotation.y,
-#     #                                           trans.transform.rotation.z,
-#     #                                           trans.transform.rotation.w))
-#     # cmd.yaw = math.degrees(yaw)
-#     pub_cmd.publish(cmd)
-
-#     # diff_x = abs(msg.pose.position.x - cmd.x)
-#     # diff_y = abs(msg.pose.position.y - cmd.y)
-
-#     #rospy.loginfo(diff_x)
-#     # if diff_x < 0.2 and diff_y < 0.2 and state == 1:
-#     #     idnt = random.randrange(1, 9)
-#     #     state = 2
-#     #     diff_x = 99
-#     #     rospy.loginfo(idnt)
-
-#     # if diff_x < 0.2 and diff_y < 0.2 and state == 2:
-#     #     idnt = random.randrange(1, 9)
-#     #     state = 3
-#     #     diff_x = 99
-    
-#     # if diff_x < 0.2 and diff_y < 0.2 and state == 3:
-#     #     idnt = random.randrange(1, 9)
-#     #     state = 1
-#     #     diff_x = 99
-
-
-# rospy.init_node('moveit')
-# sub_goal = rospy.Subscriber('/cf1/pose', PoseStamped, cmd_func)
-# # pub_cmd  = rospy.Publisher('/cf1/cmd_position', Position, queue_size=2)
-# pub_cmd  = rospy.Publisher('/cf1/cmd_hover', Hover, queue_size=2)
-# tf_buf   = tf2_ros.Buffer()
-# tf_lstn  = tf2_ros.TransformListener(tf_buf)
-
-
-# if __name__ == "__main__":
-#     global idnt, state
-
-#     rospy.spin()
---
+++ src/milestone_1_pkg/scripts/objectDetection.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import roslib
+import sys
+import rospy
+import cv2
+import numpy as np
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+class image_converter:
+
+  def __init__(self):
+    self.image_pub = rospy.Publisher("/myresult", Image, queue_size=2)
+
+    self.bridge = CvBridge()
+    self.image_sub = rospy.Subscriber("/cf1/camera/image_raw", Image, self.callback)
+
+  def callback(self,data):
+    # Convert the image from OpenCV to ROS format
+    try:
+      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+      print(e)
+
+    # Convert BGR to HSV
+    hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
+
+    # define range of the color we look for in the HSV space
+    lower_white = np.array([0,0,0])
+    upper_white = np.array([180,5,255])
+
+    lower_red = np.array([5,110,210])
+    upper_red = np.array([25,235,255])
+
+    # Threshold the HSV image to get only the pixels in ranage
+    mask_white = cv2.inRange(hsv, lower_white, upper_white)
+    mask_red = cv2.inRange(hsv, lower_red, upper_red)
+
+    # Bitwise-AND mask and original image
+    mask = mask_white + mask_red
+    res = cv2.bitwise_and(cv_image, cv_image, mask= mask)
+
+    #Take out maxi
+    max_red = np.amax(mask_red)
+    max_white = np.amax(mask_white)
+    number_of_red = np.count_nonzero(mask_red)
+    number_of_white = np.count_nonzero(mask_white)
+    #rospy.loginfo(number_of_red)
+
+
+    test = np.where(mask_red == max_red)
+    min_diag = np.min(test[0][:])
+    max_diag = np.max(test[0][:])
+
+    medel1 = np.mean(test[0][:])
+    medel2 = np.mean(test[1][:])
+    
+    cols, rows, chanels = res.shape
+    if number_of_red > 3500:
+     rospy.loginfo('Stop sign, is in the image') 
+     if cols > 100 and rows > 100 :
+      cv2.circle(img=res, center=(int(medel2),int(medel1)), radius=int(abs(max_diag-min_diag)/2), color=(255,0,0), thickness=5)
+
+
+    # Publish the image
+    try:
+      self.image_pub.publish(self.bridge.cv2_to_imgmsg(res, "bgr8"))
+    except CvBridgeError as e:
+      print(e)
+
+def main(args):
+  rospy.init_node('colorseg', anonymous=True)
+
+  ic = image_converter()
+
+  print("running...")
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("Shutting down")
+
+  cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
