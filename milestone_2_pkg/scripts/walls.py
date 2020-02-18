270723740fc0cd2f18e15514809d6495e90d2c18
KildoAlias <57793949+KildoAlias@users.noreply.github.com>
2020-02-18 13:43:21 +0100

    Update walls.py

--- milestone_2_pkg/scripts/walls.py
+++ milestone_2_pkg/scripts/walls.py
@@ -1,23 +1,55 @@
-from visualization_msgs.msg import Marker
+#!/usr/bin/env python
 import rospy
+from visualization_msgs.msg import Marker
+import json
+import numpy as np
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
 
+def publish_path(center, size):
+    marker = Marker()
+    marker.id = id
+    marker.header.frame_id = 'map'
+    marker.header.stamp = rospy.Time()
+    marker.type = 1
+    [marker.pose.orientation.x,
+    marker.pose.orientation.y,
+    marker.pose.orientation.z,
+    marker.pose.orientation.w] = quaternion_from_euler(0,0,0)
+    marker.pose.position.x = center[0]
+    marker.pose.position.y = center[1]
+    marker.pose.position.z = center[2]
+    marker.color.a = 0.5
+    marker.color.r = 1
+    marker.color.g = 1
+    marker.color.b = 1
+    marker.scale.x = size[0]
+    marker.scale.y = size[1]
+    marker.scale.z = size[2]
+    rospy.loginfo('hejhej')
+    pub_path.publish(marker)
 
-
-
-def publishWall():
-    uint8=    
-
+rospy.init_node('walls')
+pub_path = rospy.Publisher('visualization_marker', Marker, queue_size=10)
 
 def main():
+    # rate = rospy.Rate(10)  # Hz
+    cSpace=0.2
+    jsonfile="/home/k/i/kildo/dd2419_ws/src/testnodes/scripts/test.world.json"
+    with open(jsonfile, 'rb') as f:
+        world = json.load(f)
 
-    rospy.sleep(5)
+
+    for wall in world["walls"]:
+        pos_start = np.array(wall["plane"]["start"])
+        pos_stop = np.array(wall["plane"]["stop"])
+        size = np.subtract(pos_stop, pos_start)
+        center = np.divide(np.subtract(pos_stop, pos_start),2)
+        if pos_start[0]-pos_stop[0] == 0: size[0] = cSpace
+        if pos_start[1]-pos_stop[1] == 0: size[1] = cSpace
+        if pos_start[2]-pos_stop[2] == 0: size[2] = cSpace
+        publish_path(center, size)
     rospy.spin()
 
 
-rospy.init_node("walls")
-pub_cmd  = rospy.Publisher('visualization_marker', Marker, queue_size=10)
-# loadWalls()
-
-if __name__ == '__main__':
-
+if __name__=='__main__':
     main()
