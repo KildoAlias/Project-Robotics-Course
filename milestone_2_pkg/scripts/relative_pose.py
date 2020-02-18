b90e5a9cd32182cf227a08837bc7dae55afa9dfc
Tim Steinholtz <tist@kth.se>
2020-02-18 15:46:24 +0100

    Relative Pose and Arucu Trans

--- milestone_2_pkg/scripts/relative_pose.py
+++ milestone_2_pkg/scripts/relative_pose.py
@@ -4,7 +4,7 @@ import math
 import rospy
 import tf2_ros
 import tf2_geometry_msgs
-from tf.transformations import euler_from_quaternion
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
 from geometry_msgs.msg import PoseStamped, TransformStamped
 from crazyflie_driver.msg import Position
 from Astar import Astar
@@ -14,89 +14,123 @@ from aruco_msgs.msg import MarkerArray
 # Current goal (global state)
 # goal = []
 # pos = PoseStamped()
+detected = False
+count = 0
+stand_pos = True
 state = 0
-count_pose = 0
 
-def transform_camera(marker):
-    t = TransformStamped()
-    t.header.stamp = rospy.Time.now()
-    t.header.frame_id = 'cf1/camera_link'
-    t.child_frame_id = 'aruco/detected' + str(marker.id)
-    t.transform.translation = marker.pose.pose.position
-    t.transform.rotation = marker.pose.pose.orientation
-    t.transform.translation.z = t.transform.translation.z + 1
-    br2.sendTransform(t)    
 
-def transform_marker(data):
-    global x, y, z, cur_x, cur_y, cur_z, state
 
-    marker = data.markers[0]
-    transform_camera(marker)
-    frame_c = 'aruco/detected' + str(marker.id)
-    trans = tf_buf.lookup_transform('map', frame_c, rospy.Time(), rospy.Duration(1.0) )    #map, aruco/marker[id]
-    trans.header.frame_id = 'map'
-    trans.child_frame_id = 'aruco/detected_map' + str(marker.id)  #'aruco/detected' + str(marker.id)
+def aruco_position(data):
+    global detected, x, y, z, yaw
 
-    br.sendTransform(trans)
 
-    x = trans.transform.translation.x
-    y = trans.transform.translation.y
-    z = trans.transform.translation.z
-    state = 1
+    marker = PoseStamped()
+    marker.header.stamp = rospy.Time.now()
+    marker.header.frame_id = 'aruco/detected' + str(data.markers[0].id)
+    marker.pose.position.x = 0
+    marker.pose.position.y = 0.5
+    marker.pose.position.z = 0
+    
+    if not tf_buf.can_transform(marker.header.frame_id, 'map', marker.header.stamp, rospy.Duration(0.5)):
+        rospy.logwarn_throttle(5.0, 'No transform from %s to map' % marker.header.frame_id)
+        return
+
+    pose_odom = tf_buf.transform(marker, 'map', rospy.Duration(0.5))
+
+    # orientation_euler = [pose_odom.pose.pose.orientation.x, pose_odom.pose.pose.orientation.y, pose_odom.pose.pose.orientation.z, pose_odom.pose.pose.orientation.w]
+    # (ex, ey, ez) = euler_from_quaternion (orientation_euler)
+
+    x = pose_odom.pose.position.x
+    y = pose_odom.pose.position.y
+    z = pose_odom.pose.position.z
+    # rospy.loginfo(pose_odom)
+
+    roll, pitch, yaw = euler_from_quaternion((pose_odom.pose.orientation.x,
+                                              pose_odom.pose.orientation.y,
+                                              pose_odom.pose.orientation.z,
+                                              pose_odom.pose.orientation.w))
+
+    detected = True
+ 
     
 
-
-def init_pose(msg):
-    global cur_x, cur_y, cur_z
-
+def set_pose(msg):
+    global cur_x, cur_y, cur_z, cur_yaw
+    
     cur_x = msg.pose.position.x
     cur_y = msg.pose.position.y
-    cur_z = msg.pose.position.z + 0.5
+    cur_z = msg.pose.position.z
+    roll, pitch, cur_yaw = euler_from_quaternion((msg.pose.orientation.x,
+                                                msg.pose.orientation.y,
+                                                msg.pose.orientation.z,
+                                                msg.pose.orientation.w))
+   
+
+
 
 def cmd_func(msg):
-    global state, count_pose, cur_x, cur_y, cur_z
-    
-    if count_pose < 1:
-        init_pose(msg)
-        count_pose = 1
+    global detected, count, state
+
+    if count < 1:
+        set_pose(msg)
+        count = 1
+
+    # rospy.loginfo(msg)
+
+    z_dist = 0.4
+    diff_z = abs(msg.pose.position.z - z_dist)
 
     cmd = Position()
     cmd.header.stamp = rospy.Time.now()
     cmd.header.frame_id = msg.header.frame_id
 
+    if state == 0:
+        cmd.x = cur_x
+        cmd.y = cur_y
+        cmd.z = z_dist
+        cmd.yaw = cur_yaw
+        pub_cmd.publish(cmd)
+        rospy.sleep(0.2)
+
+        if diff_z < 0.05:
+            rospy.sleep(0.2)
+            set_pose(msg)
+            state = 1
+
+
     if state == 1:
-        cmd.x = x
-        cmd.y = y
-        cmd.z = z 
-        cur_x = msg.pose.position.x
-        cur_y = msg.pose.position.y
-        cur_z = msg.pose.position.z
-        state = 0
-    else:
-        cmd.x =  cur_x
-        cmd.y =  cur_y
-        cmd.z =  cur_z
+        if detected == True:
+            set_pose(msg)
+            cmd.x = x
+            cmd.y = y
+            cmd.z = z
+            cmd.yaw = yaw 
+            pub_cmd.publish(cmd)
+            detected = False
+            rospy.sleep(0.2)
+        else:
+            cmd.x = cur_x
+            cmd.y = cur_y
+            cmd.z = cur_z
+            cmd.yaw = cur_yaw
+            pub_cmd.publish(cmd)
+            rospy.sleep(0.2)
 
-    pub_cmd.publish(cmd)
 
 
-
-    
 
 if __name__ == '__main__':
     rospy.init_node("relative_pose")
 
     tf_buf   = tf2_ros.Buffer()
     tf_lstn  = tf2_ros.TransformListener(tf_buf)
-    br = tf2_ros.StaticTransformBroadcaster()
+ 
 
-    tf_buf2   = tf2_ros.Buffer()
-    tf_lstn2  = tf2_ros.TransformListener(tf_buf2)
-    br2 = tf2_ros.TransformBroadcaster()
-
-    sub_det = rospy.Subscriber('/aruco/markers', MarkerArray, transform_marker)
-    pub_cmd  = rospy.Publisher('/cf1/cmd_position', Position, queue_size=2)
+    sub_det = rospy.Subscriber('/aruco/markers', MarkerArray, aruco_position)
     sub_goal = rospy.Subscriber('/cf1/pose', PoseStamped, cmd_func)
+    pub_cmd  = rospy.Publisher('/cf1/cmd_position', Position, queue_size=2)
+    # sub_goal = rospy.Subscriber('/cf1/pose', PoseStamped, cmd_func)
 
     # listener = tf2_ros.TransformListener(tf_buf)
     
