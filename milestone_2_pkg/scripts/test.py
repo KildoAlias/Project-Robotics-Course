3eee8b678765ab93c264e94ab7dec98224a75c26
Ilian Corneliussen <ilianc@kth.se>
2020-02-19 12:29:03 +0100

    new version of test.py

--- milestone_2_pkg/scripts/test.py
+++ milestone_2_pkg/scripts/test.py
@@ -11,144 +11,113 @@ from Astar import Astar
 import matplotlib.pyplot as plt
 from aruco_msgs.msg import MarkerArray
 
-# Current goal (global state)
-# goal = []
-# pos = PoseStamped()
+
+# GLOBAL VARIABLES
 detected = False
-count = 0
 stand_pos = True
-state = 0
-cur_x = 0
-cur_y = 0
-cur_z = 0
-cur_yaw = 0
-x = 0
-y = 0
-z = 0
-yaw = 0
+pos = { 'x':0,
+        'y':0,
+        'z':0}
+goal = {'x':0,
+        'y':0,
+        'z':0,
+        'yaw':0}
 
 
-
-def aruco_position(data):
-    global detected, x, y, z, yaw
-    # print("aruco position function")
+def relativePose_callback(data):
+    # global goal, detected
+    # Wanted position in aruco detected frame. 
     marker = PoseStamped()
     marker.header.stamp = rospy.Time.now()
     marker.header.frame_id = 'aruco/detected' + str(data.markers[0].id)
     marker.pose.position.x = 0
     marker.pose.position.y = 0.8
     marker.pose.position.z = 0
-    # marker.pose.orientation = data.markers[0].pose.pose.orientation
-    # tmp = euler_from_quaternion( (marker.pose.orientation.x, marker.pose.orientation.y, marker.pose.orientation.z, marker.pose.orientation.w) ) 
-    # print("x: ", math.degrees(tmp[0]), "y: ", math.degrees(tmp[1]), "z: ", math.degrees(tmp[2]))
     [marker.pose.orientation.x,
     marker.pose.orientation.y,
     marker.pose.orientation.z,
     marker.pose.orientation.w ] = quaternion_from_euler(-90, 0, -90) 
-    # marker.pose.orientation.x = 0
-    # marker.pose.orientation.y = 0
-    # marker.pose.orientation.z = 0
-    # marker.pose.orientation.w = 0
 
+    # Look if transform is valid
     if not tf_buf.can_transform(marker.header.frame_id, 'map', marker.header.stamp, rospy.Duration(0.5)):
         rospy.logwarn_throttle(5.0, 'No transform from %s to map' % marker.header.frame_id)
         return
-
+    # Does the transform
     pose_odom = tf_buf.transform(marker, 'map', rospy.Duration(0.5))
 
 
-    x = pose_odom.pose.position.x
-    y = pose_odom.pose.position.y
-    z = pose_odom.pose.position.z
-    # rospy.loginfo(pose_odom)
-
+    goal['x'] = pose_odom.pose.position.x
+    goal['y'] = pose_odom.pose.position.y
+    goal['z'] = pose_odom.pose.position.z
     _, _, yaw = euler_from_quaternion((pose_odom.pose.orientation.x,
                                               pose_odom.pose.orientation.y,
                                               pose_odom.pose.orientation.z,
                                               pose_odom.pose.orientation.w))
-
-    yaw = math.degrees(yaw)
-    print(yaw)
+    goal['yaw'] = math.degrees(yaw)
     detected = True
 
- 
-    
 
-
-
-
-def cmd_func(msg):
-    global cur_x, cur_y, cur_z, cur_yaw
-    
-    cur_x = msg.pose.position.x
-    cur_y = msg.pose.position.y
-    cur_z = msg.pose.position.z
-    _, _, cur_yaw = euler_from_quaternion((msg.pose.orientation.x,
-                                                msg.pose.orientation.y,
-                                                msg.pose.orientation.z,
-                                                msg.pose.orientation.w))
+def getPosition_callback(msg):
+    global pos
+    pos['x'] = msg.pose.position.x
+    pos['y'] = msg.pose.position.y
+    pos['z'] = msg.pose.position.z
 
 
 rospy.init_node("relative_pose")
-sub_det = rospy.Subscriber('/aruco/markers', MarkerArray, aruco_position)
-sub_goal = rospy.Subscriber('/cf1/pose', PoseStamped, cmd_func)
+sub_det = rospy.Subscriber('/aruco/markers', MarkerArray, relativePose_callback)
+sub_goal = rospy.Subscriber('/cf1/pose', PoseStamped, getPosition_callback)
 pub_cmd  = rospy.Publisher('/cf1/cmd_position', Position, queue_size=5)
 tf_buf   = tf2_ros.Buffer()
 tf_lstn  = tf2_ros.TransformListener(tf_buf)
 
+
 def main():
-    global cur_x, cur_y, cur_z, cur_yaw, x, y, z, yaw, state, detected
+    global pos, goal, detected
     rate = rospy.Rate(10)
     z_dist = 0.4
-    tol = 0.05
+    tol_takeOff = 0.05
     tol2 = 0.2
-
+    takeOff = True
     cmd = Position()
+
     while not rospy.is_shutdown():
         
         
-        if state == 0:
+        if takeOff == True:
             print("Take off")
-            while math.sqrt((z_dist-cur_z)**2) > tol:
+            while math.sqrt((z_dist-pos['z'])**2) > tol_takeOff:
                 cmd.header.stamp = rospy.Time.now()
                 cmd.header.frame_id = "map"
                 cmd.x = 0
                 cmd.y = 0
                 cmd.z = 0.4
-                cmd.yaw = cur_yaw
+                cmd.yaw = 0
                 pub_cmd.publish(cmd)
                 rate.sleep()
             print("done")
-            state = 1
+            takeOff = False
         else:
             
             if detected == True:
-                while math.sqrt((x-cur_z)**2 + (y-cur_y)**2 + (z-cur_z)**2) > tol2:
-                    rospy.loginfo("detected")
+                rospy.loginfo("detected")
+                while math.sqrt((goal['x']-pos['x'])**2 + (goal['y']-pos['y'])**2 + (goal['z']-pos['z'])**2) > tol2:
                     cmd.header.stamp = rospy.Time.now()
                     cmd.header.frame_id = "map"
-                    cmd.x = x
-                    cmd.y = y
-                    cmd.z = z
-                    cmd.yaw = yaw 
+                    cmd.x = goal['x']
+                    cmd.y = goal['y']
+                    cmd.z = goal['z']
+                    cmd.yaw = goal['yaw'] 
                     pub_cmd.publish(cmd)
                     rate.sleep()
                 detected = False
             else:
-                # rospy.loginfo("current!")
                 cmd.header.stamp = rospy.Time.now()
                 cmd.header.frame_id = "map"
-
                 pub_cmd.publish(cmd)
                 rate.sleep()
                 
                     
-
-            
-
-
-
-
 if __name__ == '__main__':
     main()
     
